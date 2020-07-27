997e5db7ca30d7fc9af4ff9ace5e8b826d151616
Jeremy <g3raphacs@gmail.com>
2020-07-27 10:32:42 +0200

    init

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'vendor/autoload.php';
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$twig = new \Twig\Environment($loader, [
+    'cache' => false,
+    'debug' => true,
+]);
+$twig->addExtension(new \Twig\Extension\DebugExtension());
+
+$template = $twig->load('base.html.twig');
+echo $template->render(array('maVariable' => 'salut!'));
