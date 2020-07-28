0eeee914431451306e0d19033e684e696377447c
Jeremy Mathieu <g3raphacs@gmail.com>
2020-07-28 15:17:03 +0200

    hide/show function for modal

--- js/script.js
+++ js/script.js
@@ -3,8 +3,7 @@ const title =document.getElementById('title');
 const catList =document.getElementById('categorieList');
 
 title.addEventListener('click' , function(){
-    console.log('click')
-    catList.classList.toggle('hidden')
+    catList.classList.toggle('hidden');
 })
 
 //modale
@@ -13,12 +12,44 @@ const modalClose = document.getElementById('modal-close');
 
 function loadModal(id){
     modal.classList.add('active')
-    console.log('loadmodal');
 }
 function clickDelete(id){
     modal.classList.remove('active');
-    console.log('clickdelete');
 }
 modalClose.addEventListener('click' , function(){
     modal.classList.remove('active');
 })
+
+//switchmode
+const staticElements = document.getElementsByClassName('m-static');
+const modifElements = document.getElementsByClassName('m-modif');
+
+window.onload=function(){
+    for(let i = 0 ; i<staticElements.length ; i++){
+        staticElements[i].classList.remove('hide');
+    }
+    for(let i = 0 ; i<modifElements.length ; i++){
+        modifElements[i].classList.add('hide');
+    }
+}
+
+function modifActivate(){
+    console.log('activate')
+    console.log(staticElements.length)
+    for(let i = 0 ; i<staticElements.length ; i++){
+        console.log(staticElements[i]);
+        staticElements[i].classList.add('hide');
+    }
+    for(let i = 0 ; i<modifElements.length ; i++){
+        modifElements[i].classList.remove('hide');
+    }
+}
+function modifValidate(){
+    console.log(staticElements);
+    for(let i = 0 ; i<staticElements.length ; i++){
+        staticElements[i].classList.remove('hide');
+    }
+    for(let i = 0 ; i<modifElements.length ; i++){
+        modifElements[i].classList.add('hide');
+    }
+}
