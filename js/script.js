6201ff20254835541e9e25211d601ad8068067f8
Jeremy Mathieu <g3raphacs@gmail.com>
2020-07-28 14:19:13 +0200

    add product modal

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
+// liste categories
 const title =document.getElementById('title');
-console.log(title)
 const catList =document.getElementById('categorieList');
 
 title.addEventListener('click' , function(){
     console.log('click')
     catList.classList.toggle('hidden')
+})
+
+//modale
+const modal = document.getElementById('product-modal');
+const modalClose = document.getElementById('modal-close');
+
+function loadModal(id){
+    modal.classList.add('active')
+    console.log('loadmodal');
+}
+function clickDelete(id){
+    modal.classList.remove('active');
+    console.log('clickdelete');
+}
+modalClose.addEventListener('click' , function(){
+    modal.classList.remove('active');
 })
