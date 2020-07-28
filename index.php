ba0d0ac1a72598fe234b23e039bfb53b6533fe8e
AlxandrV <a.vigneaud@gmail.com>
2020-07-28 10:51:02 +0200

    Add - list produits

--- index.php
+++ index.php
@@ -9,16 +9,26 @@ $twig->addExtension(new \Twig\Extension\DebugExtension());
 
 // Liste des catégories
 function categories(){
-    require_once 'bdd.php';
+    require 'bdd.php';
 
     $sql = 'SELECT categorie FROM categories';
     $req = $bdd -> query($sql);
     return $req;
-} 
+}
+
+// Liste des produits
+function produits_list(){
+    require 'bdd.php';
+
+    $sql = 'SELECT p.id, p.nom, p.reference, p.prix, c.categorie FROM produits AS p INNER JOIN categories AS c ON p.id_categorie = c.id';
+    $req = $bdd -> query($sql);
+    return $req;
+}
 
 $template = $twig->load('base.html.twig');
 echo $template->render(array(
     'user' => 'Admin',
     'categorieTitle' => 'Titre de la catégorie',
     'categories' => categories(),
+    'produits_list' => produits_list()
 ));
