43ca251924f3e104ea5b4bf72c1170584680faad
AlxandrV <a.vigneaud@gmail.com>
2020-07-28 19:37:17 +0200

    Add - order by date or price

--- back/filtre.php
+++ back/filtre.php
@@ -2,8 +2,23 @@
 
 // Si $_GET['categorie'] n'existe pas vaut toute catégorie
 if(!isset($_GET['categorie']) || empty($_GET['categorie'])){
-    $select_categorie = 'Choisissez une catégorie';
+    $select_categorie = '';
 }
 else{
     $select_categorie = $_GET['categorie'];
+}
+
+// Définie l'ordre d'affichage des produits
+if(isset($_GET['order']) || !empty($_GET['order'])){
+    if($_GET['order'] == 'date_croissant'){
+        $order = 'date_croissant';
+    }elseif($_GET['order'] == 'date_decroissant'){
+        $order = 'date_decroissant';
+    }elseif($_GET['order'] == 'prix_croissant'){
+        $order = 'prix_croissant';
+    }elseif($_GET['order'] == 'prix_decroissant'){
+        $order = 'prix_decroissant';
+    }
+}else{
+    $order = '';
 }
--- index.php
+++ index.php
@@ -6,6 +6,7 @@ $twig = new \Twig\Environment($loader, [
     'debug' => true,
     ]);
     $twig->addExtension(new \Twig\Extension\DebugExtension());
+
     
 // Pagination
 require 'back/pagination.php';
@@ -13,46 +14,14 @@ require 'back/pagination.php';
 // Tri par catégorie
 require 'back/filtre.php';
 
-// Liste des catégories
-function categories(){
-    require 'back/bdd.php';
-
-    $sql = 'SELECT categorie FROM categories';
-    $req = $bdd -> query($sql);
-    return $req;
-}
-
-// Liste des produits
-function produits_list(){
-    // Récupère le le numéro de page et la limite pour afficher les produits
-    require 'back/pagination.php';
-
-    // Récupère la valeur de $select_categorie
-    require 'back/filtre.php';
-
-    if($select_categorie == 'Choisissez une catégorie'){
-        $sql = 'SELECT p.id, p.nom, p.reference, p.prix, p.date_achat, p.date_fin_garantie, c.categorie FROM produits AS p INNER JOIN categories AS c ON p.id_categorie = c.id ORDER BY id  ASC LIMIT :debut, :limit';
-
-        $req = $bdd -> prepare($sql);
-
-    } else{
-        $sql = 'SELECT p.id, p.nom, p.reference, p.prix, p.date_achat, p.date_fin_garantie, c.categorie FROM produits AS p INNER JOIN categories AS c ON p.id_categorie = c.id WHERE c.categorie = :categorie ORDER BY id  ASC LIMIT :debut, :limit';
-
-        $req = $bdd -> prepare($sql);
-        $req -> bindValue('categorie', $select_categorie, PDO::PARAM_STR);
-    }
-
-    $req -> bindValue('debut', $debut, PDO::PARAM_INT);
-    $req -> bindValue('limit', $limit, PDO::PARAM_INT);
-    $req -> execute();
-
-    return $req;
-}
+// Liste des fonction
+require 'back/function.php';
 
 $template = $twig->load('base.html.twig');
 echo $template->render(array(
     'user' => 'Admin',
     'categorieTitle' => $select_categorie,
+    'order' => $order,
     'categories' => categories(),
     'produits_list' => produits_list(),
     'nombre_page' => $nombre_page,
