96574249bbbe761929aaad10f6048f06787fe86c
Jeremy <g3raphacs@gmail.com>
2020-07-30 16:45:15 +0200

    ajax

--- back/ajax_produit_by_id.php
+++ back/ajax_produit_by_id.php
@@ -3,5 +3,4 @@ require 'function.php';
 
 if(isset($_GET['id'])){
     echo detail_modal($_GET['id']);
-    
 }
--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // liste categories
 const title =document.getElementById('title');
 const catList =document.getElementById('categorieList');
+const prod_Vendor_Type_input = document.getElementById('prod_Vendor_Type_input');
+let product_id = 0;
 
 title.addEventListener('click' , function(){
     catList.classList.toggle('hidden');
@@ -9,13 +11,108 @@ title.addEventListener('click' , function(){
 //modale
 const modal = document.getElementById('product-modal');
 const modalClose = document.getElementById('modal-close');
+let idProduit = 0;
 
 function loadModal(id){
+    product_id=id;
     catList.classList.add('hidden')
     modal.classList.add('active')
     vendorType();
+
+    const prod_Picture = document.getElementById('prod_Picture');
+    const prod_Ticket = document.getElementById('prod_Ticket');
+    const prod_Name = document.getElementById('prod_Name');
+    const prod_Name_input = document.getElementById('prod_Name_input');
+    const prod_Serial = document.getElementById('prod_Serial');
+    const prod_Serial_input = document.getElementById('prod_Serial_input');
+    const prod_Categorie = document.getElementById('prod_Categorie');
+    const prod_Categorie_input = document.getElementById('prod_Categorie_input');
+    const prod_Price = document.getElementById('prod_Price');
+    const prod_Price_input = document.getElementById('prod_Price_input');
+    const prod_Date = document.getElementById('prod_Date');
+    const prod_Date_input = document.getElementById('prod_Date_input');
+    const prod_Warranty = document.getElementById('prod_Warranty');
+    const prod_Warranty_input = document.getElementById('prod_Warranty_input');
+    const prod_Vendor_Name = document.getElementById('prod_Vendor_Name');
+    const prod_Vendor_Name_input = document.getElementById('prod_Vendor_Name_input');
+    const prod_Vendor_Address = document.getElementById('prod_Vendor_Address');
+    const prod_Vendor_Street_input = document.getElementById('prod_Vendor_Street_input');
+    const prod_Vendor_Code_input = document.getElementById('prod_Vendor_Code_input');
+    const prod_Vendor_City_input = document.getElementById('prod_Vendor_City_input');
+    const prod_Vendor_URL = document.getElementById('prod_Vendor_URL');
+    const prod_Vendor_URL_input = document.getElementById('prod_Vendor_URL_input');
+    const prod_Tips = document.getElementById('prod_Tips');
+    const prod_Tips_input = document.getElementById('prod_Tips_input');
+
+    const formData = new FormData();
+    formData.append('id', JSON.stringify(id));
+
+    fetch( 'back/ajax_produit_by_id.php', { method : "post" , body : formData } )
+        .then( res => res.json() ).then( data =>{
+
+        // image
+            if(data.picture[0]!=false){
+                prod_Picture.setAttribute('src' , data.picture[0]);
+            }
+            else{
+                prod_Picture.setAttribute('src' , 'images/product-main/placeholder.jpg');
+            }
+
+        // ticket
+            if(data.ticket!=""){
+                prod_Picture.setAttribute('src' , data.ticket);
+            }
+            else{
+                prod_Picture.setAttribute('src' , 'images/product-ticket/placeholder.jpg');
+            }
+
+        //nom produit
+            prod_Name.innerText = data.name;
+            prod_Name_input.value = data.name;
+
+        //numero de serie
+            prod_Serial.innerText = data.serial;
+            prod_Serial_input.value = data.serial;
+
+        //categorie
+            prod_Categorie.innerText = data.categorie;
+            prod_Categorie_input.value = data.categorie_id;
+
+        //prix
+            prod_Price.innerText = data.price;
+            prod_Price_input.value = data.price;
+
+        //prix
+            prod_Date.innerText = data.date;
+            prod_Date_input.value = data.date;
+
+        //garantie
+            prod_Warranty.innerText = data.warranty;
+            prod_Warranty_input.value = data.warranty;
+
+        //vendeur nom
+            prod_Vendor_Name.innerText = data.vendor_name;
+            prod_Vendor_Name_input.value = data.vendor_name;
+
+        // vendeur adresse
+            prod_Vendor_Address.innerText = data.vendor_street+" - "+data.vendor.code+" - "+data.vendor.city;
+            prod_Vendor_Name_input.value = data.vendor_name;
+            prod_Vendor_Street_input.value = data.vendor_street;
+            prod_Vendor_Code_input.value = data.vendor_code;
+            prod_Vendor_City_input.value = data.vendor_city;
+
+        //vendeur url
+            prod_Vendor_URL.innerText = data.vendor_url;
+            prod_Vendor_URL_input.value = data.vendor_url;
+
+        //conseils
+            prod_Tips.innerText = data.vendor_url;
+            prod_Tips_input.value = data.vendor_url;
+
+        })
+
 }
-function clickDelete(id){
+function clickDelete(){
     modal.classList.remove('active');
 }
 modalClose.addEventListener('click' , function(){
@@ -62,15 +159,13 @@ function modifValidate(){
 }
 
 //vendor type
-
-const vendorTypeInput = document.getElementById('form-vendor');
 function vendorTypeChange(){
     vendorType();
 }
 
 function vendorType(){
 
-    let type = vendorTypeInput.value;
+    let type = prod_Vendor_Type_input.value;
     const vendorElements1 = document.getElementsByClassName('vendor1')
     const vendorElements2 = document.getElementsByClassName('vendor2')
     if(type==1){
@@ -111,7 +206,7 @@ deleteNo.addEventListener('click' , function(){
     deleteModal.classList.remove('active');
 })
 
-function deleteProduct(index){
+function deleteProduct(){
     deleteModal.classList.remove('active');
 }
 
@@ -124,10 +219,10 @@ modalUppicClose.addEventListener('click' , function(){
     uppicModal.classList.remove('active');
 })
 
-function uppicValidate(index){
+function uppicValidate(){
     uppicModal.classList.remove('active');
 }
-function uploadPictureClick(index){
+function uploadPictureClick(){
     uppicModal.classList.add('active');
 }
 
@@ -139,10 +234,10 @@ modalUpTicketClose.addEventListener('click' , function(){
     upTicketModal.classList.remove('active');
 })
 
-function upticketValidate(index){
+function upticketValidate(){
     upTicketModal.classList.remove('active');
 }
-function uploadTicketClick(index){
+function uploadTicketClick(){
     upTicketModal.classList.add('active');
 }
 
