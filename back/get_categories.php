29f43f1ccb19295c31faf3aaa3a5552adacd7e37
Jeremy <g3raphacs@gmail.com>
2020-08-23 13:19:59 +0200

    bug correction on upload and categories

--- back/get_categories.php
+++ back/get_categories.php
@@ -1,20 +1,4 @@
 <?php
-    // require 'bdd.php';
-    // $array = array();
-    // // liste des catégories
-    // $sql = 'SELECT categorie FROM categories';
-    // $categorie = $bdd -> query($sql);
-
-    // $array_categories = [];
-    // while($donnees = $categorie->fetchColumn()){
-    //     array_push($array_categories, $donnees);
-    // }
-
-    // $categories = ["categories" => $array_categories];
-    // array_push($array, $categories);
-
-    // echo json_encode($array);
-
 
     require 'bdd.php';
     $array = array();
@@ -29,9 +13,6 @@
         array_push($array_id, $donnees['id']);
     }
 
-    // $categories = ["categories" => $array_categories,
-    // "categories_id" => $array_categories
-    // ];
     array_push($array, $array_categories);
     array_push($array, $array_id);
 
